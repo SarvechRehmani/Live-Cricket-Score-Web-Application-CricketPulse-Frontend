2f1a280a1c73be6a8718ca622a8e1a4b70bec127
Sarvech <sarwech99@gmail.com>
2024-07-25 19:36:51 +0500

    adding router to the project.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LiveComponent } from './pages/live/live.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { PointTableComponent } from './pages/point-table/point-table.component';
+import { UpcomingComponent } from './pages/upcoming/upcoming.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'live',
+    component: LiveComponent,
+  },
+  {
+    path: 'history',
+    component: HistoryComponent,
+  },
+  {
+    path: 'point-table',
+    component: PointTableComponent,
+  },
+  {
+    path: 'upcoming',
+    component: UpcomingComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
